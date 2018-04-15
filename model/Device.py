76262bd3f01df4558363499d00b2bfff14e9565b
tony <tony@localhost.localdomain>
2018-04-15 17:00:06 -0600

    database migration

--- model/Device.py
+++ model/Device.py
@@ -16,9 +16,11 @@ class Device(Base):
     id = Column(Integer, primary_key=True)
     device = Column(Text)
     max_flow = Column(Integer)
+    status = Column(Text)
 
     def __init__(self, name, flow=1000):
         self.id = Device.stored_id
         Device.stored_id += 1
         self.name = name
         self.flow = flow
+        self.status = "on"
